7043fa296bbd6539a7dca40c19b803ce76c1e6b3
Allen Rosales <arosales9@my.bcit.ca>
2025-03-05 14:12:40 -0800

    EDIT

---
+++ scripts/firebaseAPI_DTC14.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPZrYSBuHFEO9lzlpgSwWm7D0hnS2GBv0",
+    authDomain: "dtc14-16617.firebaseapp.com",
+    projectId: "dtc14-16617",
+    storageBucket: "dtc14-16617.firebasestorage.app",
+    messagingSenderId: "172892212148",
+    appId: "1:172892212148:web:ca31a8cd98deb5580a40e1"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();t 
