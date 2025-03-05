8109e73abe587cc18950dcbe23e62f6dd92d3350
Allen Rosales <arosales9@my.bcit.ca>
2025-03-05 15:46:56 -0800

    EDITS

--- scripts/firebaseAPI_DTC14.js
+++ scripts/firebaseAPI_DTC14.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCPZrYSBuHFEO9lzlpgSwWm7D0hnS2GBv0",
-    authDomain: "dtc14-16617.firebaseapp.com",
-    projectId: "dtc14-16617",
-    storageBucket: "dtc14-16617.firebasestorage.app",
-    messagingSenderId: "172892212148",
-    appId: "1:172892212148:web:ca31a8cd98deb5580a40e1"
+    apiKey: "AIzaSyBzEt57-mIMouR07C6iBzNt4yL5brGCv4c",
+    authDomain: "communitycart-696b4.firebaseapp.com",
+    projectId: "communitycart-696b4",
+    storageBucket: "communitycart-696b4.firebasestorage.app",
+    messagingSenderId: "74776250719",
+    appId: "1:74776250719:web:a0ff143fcf7283d9364eea"
 };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();t 
+const db = firebase.firestore(); 
--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,38 @@
+$(document).ready(setup)
+function insertNameFromFirestore() {
+  // Check if the user is logged in:
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+      currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+      currentUser.get().then(userDoc => {
+        // Get the user name
+        let userName = userDoc.data().name;
+        console.log(userName);
+        //$("#name-goes-here").text(userName); // jQuery
+        document.getElementById("name-goes-here").innerHTML = userName;
+      })
+    } else {
+      console.log("No user is logged in."); // Log a message when no user is logged in
+    }
+  })
+}
+
+insertNameFromFirestore();
+
+
+
+function home() {
+  console.log('in home')
+  console.log('in home');
+  let post1 = $('.post-1').clone();  // Clone the template post
+
+  for (let i = 0; i < 5; i++) {
+    $('.posts').append(post1.clone()); // Append a fresh clone each time
+  }
+
+}
+
 const posts = [
   {
     id: 1,
@@ -21,22 +56,9 @@ const posts = [
   }
 ];
 
-
-function home(){
-    console.log('in home')
-       console.log('in home');
-    let post1 = $('.post-1').clone();  // Clone the template post
-
-    for (let i = 0; i < 5; i++) {
-        $('.posts').append(post1.clone()); // Append a fresh clone each time
-    }
-    
-}
-
-function setup(){
-home()
+function setup() {
+  home()
 }
 
 
 
-$(document).ready(setup)
