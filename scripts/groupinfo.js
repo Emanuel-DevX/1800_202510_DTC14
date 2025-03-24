a466af21667c5314af66a09fc3ef6542dfa04993
Allen Rosales <arosales9@my.bcit.ca>
2025-03-24 12:44:16 -0700

    trip pages members

--- scripts/groupinfo.js
+++ scripts/groupinfo.js
@@ -1,5 +1,4 @@
 function getUserName(owner) {
-    console.log(123)
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             db.collection("users").doc(owner).get()
@@ -19,6 +18,42 @@ function getUserName(owner) {
 }
 
 
+function membersNames(member_list){
+    console.log(member_list)
+    var empty_string = '';
+    for (let y of member_list) {
+        console.log(123);
+        empty_string += `${y} ~ `;
+    }
+    console.log(empty_string);
+    document.getElementById("group_members").innerText = empty_string;
+}
+
+function getMemberName(member) {
+    var member_list = [];
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            for (let x of member) {
+                db.collection("users").doc(x).get()
+                    .then((doc) => {
+                        if (doc.exists) {
+                            var member_name = doc.data().name;
+
+                            member_list.push(member_name)
+
+                            membersNames(member_list)
+                        } else {
+                            console.log("No such document!");
+                        }
+                    })
+            }
+            
+        } else {
+            console.log("No user is logged in");
+        }
+    });
+}
+
 function getInfo() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -32,16 +67,16 @@ function getInfo() {
                         var owner = doc.data().owner;
                         var category = doc.data().category;
                         var date = doc.data().deadline;
-                        var members = doc.data().members;
+                        var member = doc.data().members;
 
-                    
+
                         document.getElementById("group_name").innerText = name;
                         document.getElementById("group_description").innerText = description;
                         document.getElementById("group_category").innerText = category;
                         document.getElementById("group_date").innerText = date;
-                        document.getElementById("group_members").innerText = members;
 
                         getUserName(owner)
+                        getMemberName(member)
                     } else {
                         console.log("No such document!");
                     }
