0aec13ba099e44926caf0f30b866a77d9f13b93d
Allen Rosales <arosales9@my.bcit.ca>
2025-04-01 13:39:05 -0700

    removed some pages

--- scripts/contact.js
+++
@@ -1,6 +0,0 @@
-function setup(){
-    document.getElementById(`back-btn`).addEventListener("click", () => {
-        document.location.href = "home.html"
-    })
-}
-document.addEventListener("DOMContentLoaded", setup);
